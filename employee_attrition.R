62d5743f33433104c3fd78234b65774fc950be2c
Ricky0625 <75558748+Ricky0625@users.noreply.github.com>
2022-03-21 10:11:24 +0800

    Delete employee_attrition.R

--- employee_attrition.R
+++
@@ -1,9 +0,0 @@
-# CT127-3-2 PFDA
-# Ricky Wong Tiong Song
-# TP054622
-
-# Set the working directory
-# getwd()
-# setwd("D:\\Desktop\\Degree Y2\\SEM 1\\PFDA\\Assignment\\PFDA Assignment")
-data <- read.csv("employee_attrition.csv")
-View(data)
